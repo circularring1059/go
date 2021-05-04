760777436048cb34369a9d1a5072e3162446de54
原件 <ring@163.com>
2021-05-04 22:39:20 +0800

    add const

---
+++ base/var/day2/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+/*其他玩法*/
+//定义枚举
+const (
+	one   int = 1
+	two   int = 2
+	three int = 3
+)
+
+//配合iota  使用
+const (
+	a = iota // iota  初始值为0 a = 0
+	b        // b = 1
+	c        // c = 2
+)
+
+const (
+	d, e = iota + 2, iota * 2 //d = 2 e = 0
+	f, g                      //f =3 g =2
+
+	h, k = iota * 3, iota + 1 // h = 6 k = 3
+
+)
+
+func main() {
+	//关键字 const  定义常量（只读属性）
+	const name string = "google.com" //用双引号
+	fmt.Println(name)
+
+	//name = "baidul.com"   常量不可以被修改
+
+	fmt.Println(one, two, three)
+
+	fmt.Println(a, b, c)
+
+	fmt.Println(d, e, f, g, h, k)
+}
