cde80d974180022b6ffa21b3bd424825426bff5b
yuanjiancheng <yuan.jiancheng@ricequant.com>
2021-05-06 21:17:27 +0800

    update

---
+++ base/controller/day1/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	fmt.Println("-------------")
+
+	var j int = 0
+	for ; j < 9; j++ {
+		fmt.Println(j)
+	}
+
+	fmt.Println("--------------")
+	for ; j < 15; j++ {
+		fmt.Println(j)
+		j++
+	}
+}
