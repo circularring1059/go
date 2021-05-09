f72387241fe446084465b43fb323f4da9ce95f43
原件 <ring@163.com>
2021-05-09 22:30:26 +0800

    update

--- base/dataStructure/array/day1/main.go
+++ base/dataStructure/array/day1/main.go
@@ -7,6 +7,8 @@ func main() {
 	var array1 = [2]int{} //表示数组长度为2,容量为2 内容为int 默认是[0 0]
 	fmt.Println(array1)   //[0 0]
 	fmt.Println(len(array1), cap(array1))
+	array1 = [2]int{1, 2}
+	fmt.Println(array1)
 
 	var array2 = [3]string{"h", "e"}
 	var array3 = [4]int{1, 2, 3}
@@ -17,7 +19,10 @@ func main() {
 	fmt.Println(array4)             //[4 0 0 0 5]
 
 	var array5 = [...]int{1, 3, 5, 7, 9}
-	fmt.Println(array5) //[1 3 5 7 9]
-	fmt.Printf("%v %v\n", len(array5), cap(array5))
+	fmt.Println(array5)                             //[1 3 5 7 9]
+	fmt.Printf("%v %v\n", len(array5), cap(array5)) //5 5
+
+	var array6 = [5]int{0: 2, 1: 3, 2: 10, 3: 100, 4: 7}
+	fmt.Println(array6)
 
 }
