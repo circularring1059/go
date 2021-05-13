2742ef361e94c21305beb1c6d896fe23445e5fd3
yuanjiancheng <yuan.jiancheng@ricequant.com>
2021-05-13 20:22:14 +0800

    update

--- base/array/day2/main.go
+++ base/array/day2/main.go
@@ -31,11 +31,18 @@ func main() {
 	}
 
 	//切片长度与容量
-	var slice4 = make([]int, 2, 5)                      //有两个元素(int 默认是0，现在可以放十个
-	fmt.Println(slice4)                                 // [0 0]
-	fmt.Printf("长度:%v 容量:%v", len(slice4), cap(slice4)) //长度:2 容量:5
-	//增加元素
-	slice4[3] = 1
-	fmt.Println(slice4)
+	var slice4 = make([]int, 2, 5)                        //有两个元素(int 默认是0，现在可以放十个
+	fmt.Println(slice4)                                   // [0 0]
+	fmt.Printf("长度:%v 容量:%v\n", len(slice4), cap(slice4)) //长度:2 容量:5
+
+	//声明一个  slice5 == nil
+	var slice5 []int
+	fmt.Printf("%v %d %d\n", slice5, len(slice5), cap(slice5)) // [] 0 0
+	// slice5[0] = 3 // fmt.Printf("%v %d %d", slice5, len(slice5), cap(slice5))  没有容量不能赋值 runtime error: index out of range [0] with length 0
+	slice5 = append(slice5, 7878)
+	fmt.Printf("%v %d %d\n", slice5, len(slice5), cap(slice5)) //append  后自动开辟空间  [7878] 1 1
+
+	slice5[0] = 9898 //修改值
+	fmt.Println(slice5)
 
 }
