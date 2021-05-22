2919736e9db8e8f98cec5870788931f01fc9731e
原件 <ring@163.com>
2021-05-22 18:05:52 +0800

    update

--- base/function/day10/main.go
+++ base/function/day10/main.go
@@ -5,17 +5,19 @@ import "fmt"
 
 func calc(x, y int) (func() int, func() int) {
 	fmt.Println(x, y)
-	add := func() int {
-		return x + y
-	}
+	// add := func() int {
+	// 	return x + y
+	// }
 
 	sub := func() int {
 		return x - y
 	}
-	return add, sub
+	return func() int {
+		return x + y
+	}, sub
 }
 
 func main() {
 	add, sub := calc(3, 1)
-	fmt.Println(add(), sub())
+	fmt.Println(add(), sub()) // 4 2
 }
