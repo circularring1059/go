085655565f987775c64591ed22610484ad5f343e
yuanjiancheng <yuan.jiancheng@ricequant.com>
2021-05-27 21:19:07 +0800

    mupdate

---
+++ base/function/day13/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("starting golang")
+	// var z int32
+	// z = '中'
+	// fmt.Print(z)
+	var z = '1'
+	fmt.Printf("%c", z)
+	fmt.Print(z)
+
+}
