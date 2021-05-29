5ae844d156a88eb844acccea2a3051ac3dbb21e3
原件 <ring@163.com>
2021-05-29 17:08:36 +0800

    update

---
+++ base/struct/day1/main.go
@@ -0,0 +1,22 @@
+//struct  --->> 类
+package main
+
+import "fmt"
+
+type person struct {
+	name, gender string
+	age          int
+	hobby        []string
+}
+
+func main() {
+	var jack person
+	fmt.Printf("%v %T\n", jack, jack) //{  0 []} main.person
+
+	jack.name = "jack"
+	jack.gender = "男"
+	jack.age = 19
+	jack.hobby = []string{"football", "basketball"}
+
+	fmt.Println(jack) //
+}
