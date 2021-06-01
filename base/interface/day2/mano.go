553e7a2efc251d62aa65baa079c2f80bf96e9325
yuanjiancheng <yuan.jiancheng@ricequant.com>
2021-06-01 21:02:54 +0800

    update

--- base/interface/day2/mano.go
+++ base/interface/day2/mano.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type calc interface {
+	add(x, y int) (z int)
+
+	// sub(x, y int) (z int) {
+	// 	if  x > y {
+	// 		return x -y
+	// 	}else {
+	// 		return y -x
+	// 	}
+	// }
+
+}
+
+type computer struct {
+	brand string
+}
+
+func (c computer) add(x, y int) (z int) {
+	return x + y
+}
+
+func h(i calc, x int, y int) {
+	i.add(x, y)
+}
 
 func main() {
 	fmt.Println("Try to do it")
+	var leavel = computer{
+		"leavle",
+	}
+
+	var t calc
+	t = leavel
+	fmt.Println(t.add(3, 7))
+
+	s := h(leavel, 2, 4)
+	fmt.Println(s)
+
 }
