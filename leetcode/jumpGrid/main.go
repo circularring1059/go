392e0e6ecd19a0e5384f96766467ed7ff98f1a91
原件 <ring@163.com>
2022-01-03 23:37:30 +0800

    update

--- leetcode/jumpGrid/main.go
+++ leetcode/jumpGrid/main.go
@@ -13,22 +13,22 @@ func jumpGrid(m, n int) int {
 	// var (
 	// 	count, left, right int
 	// )
-	fmt.Println(m, n)
+	// fmt.Println(m, n)
 	if m == 0 && n == 0 {
-		fmt.Println("over")
-		// count++
-		// return count
+		// fmt.Println("over")
+		count++
 	}
 	if m > 0 {
-		return jumpGrid(m-1, n)
+		jumpGrid(m-1, n)
 	}
 
 	if n > 0 {
-		return jumpGrid(m, n-1)
+		// fmt.Println(m, n)
+		jumpGrid(m, n-1)
 	}
 	return count
 }
 
 func main() {
-	fmt.Println(jumpGrid(1, 1))
+	fmt.Println(jumpGrid(2, 2))
 }
