4a83a88122a8d3c18775ff5ec80a74dbdccc2d80
yuanjiancheng <yuan.jiancheng@ricequant.com>
2022-01-04 16:27:29 +0800

    add  brackdetCombinations

---
+++ leetcode/bracketsCombinations/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+var str string
+var ret = make([]string, 0)
+
+func gen(n int, str string, left, right int) []string {
+	if len(str) == 2*n {
+		ret = append(ret, str)
+	}
+	if left < n {
+		gen(n, str+"(", left+1, right)
+	}
+	if right < left {
+		gen(n, str+")", left, right+1)
+	}
+	return ret
+}
+
+func main() {
+	fmt.Println(gen(2, str, 0, 0))
+}
