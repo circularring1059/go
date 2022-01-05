10cd9cef946cf5c4853e08c91cadc6a45c452a6a
yuanjiancheng <yuan.jiancheng@ricequant.com>
2022-01-05 10:29:16 +0800

    add  packageOflist

---
+++ base/packgeOflist/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+func main() {
+	l1 := list.New()
+	fmt.Println(l1)
+
+	l1.PushBack("first") //append
+	l1.PushBack("second")
+	element := l1.PushBack("three")
+	fmt.Println("*", element)
+	l1.InsertBefore("world", element)
+
+	l1.PushBack("end")
+	element1 := l1.PushFront(1) //头插法  element  相当于index?
+	element2 := l1.InsertAfter("hello", element1)
+	l1.Remove(element2)
+
+	//remove
+
+	for i := l1.Front(); i != nil; i = i.Next() {
+		fmt.Println(i.Value)
+
+	}
+}
