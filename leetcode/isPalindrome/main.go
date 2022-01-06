759ed3ebea6d5d175aba9f20705c7d9d0e74f764
yuanjiancheng <yuan.jiancheng@ricequant.com>
2022-01-06 17:30:34 +0800

    update

--- leetcode/isPalindrome/main.go
+++ leetcode/isPalindrome/main.go
@@ -20,10 +20,28 @@ func isPalindrome(num int) bool {
 		return false
 	}
 	return true
+}
 
+func isPalindrome1(num int) bool {
+	fmt.Printf("%T", num)
+	if num < 0 {
+		return false
+	}
+	num1 := num
+	reverse, tmp := 0, 0
+	for num > 0 {
+		num, tmp = test(num)
+		reverse = reverse*10 + tmp
+	}
+	return reverse == num1
+}
+func test(num int) (x, y int) {
+	return num / 10, num % 10
 }
 
 func main() {
 	num := -121
 	fmt.Println(isPalindrome(num))
+	fmt.Println(test(101))
+	fmt.Println(isPalindrome1(01))
 }
