2c9a8acc35dcb0010dd587ae476ed6a9731ded3d
原件 <ring@163.com>
2022-01-09 13:30:43 +0800

    add goto

--- base/label/main.go
+++ base/label/main.go
@@ -28,7 +28,7 @@ LABEL2:
 	for i := 0; i <= 5; i++ {
 		for j := 0; j <= 5; j++ {
 			if j == 4 {
-				break LABEL2
+				break LABEL2 //跳出整个循环
 			}
 			fmt.Printf("i is: %d, and j is: %d\n", i, j)
 		}
---
+++ base/switch/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 1
+	switch a {
+	case 10:
+		fmt.Println("**")
+	case 20:
+		fmt.Println(20)
+	default:
+		fmt.Println("default")
+	}
+
+}
