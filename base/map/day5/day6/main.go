4497c788c65853960acf7ec1d5616eb3fc4c56a5
yuanjiancheng <yuan.jiancheng@ricequant.com>
2022-01-12 09:04:37 +0800

    update

---
+++ base/map/day5/day6/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	map1 := map[int]int{3: 1, 5: 2, 1: 4}
+	fmt.Println(map1)
+	for k, v := range map1 {
+		fmt.Println(k, v)
+	}
+
+	keys := make([]int, len(map1))
+
+	i := 0
+	for k, _ := range map1 {
+		keys[i] = k
+		i++
+	}
+
+	fmt.Println(keys)
+	sort.Ints(keys)
+	fmt.Println(keys)
+
+	for _, i := range keys {
+		fmt.Println(i, map1[i])
+	}
+}
