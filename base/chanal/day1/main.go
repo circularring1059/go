c1d8fa046f0c649e9c6dc2a62fef8f6a2ba215cf
yuanjiancheng <yuan.jiancheng@ricequant.com>
2022-01-13 13:55:06 +0800

    update

--- base/chanal/day1/main.go
+++ base/chanal/day1/main.go
@@ -1,48 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func main() {
-	fmt.Println("chanal")
-	ch := make(chan int) //无缓冲
-	// ch <- 2              //error
+// func main() {
+// 	fmt.Println("chanal")
+// 	// ch := make(chan int) //无缓冲
+// 	// ch <- 2              //error
 
-	a := <-ch
-	fmt.Println(a)
-	// fmt.Println("*")
-	// ch <- 3
-	// fmt.Println("**")
-}
+// 	// a := <-ch  //error
+// 	// fmt.Println(a)
+// 	// fmt.Println("*")
+// 	// ch <- 3
+// 	// fmt.Println("**")
+// }
 
 // package main
 
-// import (
-// 	"fmt"
-// 	"time"
-// )
+func main() {
+	ch := make(chan string)
 
-// func main() {
-// 	ch := make(chan string)
+	go sendData(ch)
+	go getData(ch)
 
-// 	go sendData(ch)
-// 	go getData(ch)
+	time.Sleep(1e9)
+}
 
-// 	time.Sleep(1e9)
-// }
+func sendData(ch chan string) {
+	ch <- "Washington"
+	ch <- "Tripoli"
+	ch <- "London"
+	ch <- "Beijing"
+	ch <- "Tokio"
+}
 
-// func sendData(ch chan string) {
-// 	ch <- "Washington"
-// 	ch <- "Tripoli"
-// 	ch <- "London"
-// 	ch <- "Beijing"
-// 	ch <- "Tokio"
-// }
-
-// func getData(ch chan string) {
-// 	var input string
-// 	// time.Sleep(2e9)
-// 	for {
-// 		input = <-ch
-// 		fmt.Printf("%s\n", input)
-// 	}
-// }
+func getData(ch chan string) {
+	var input string
+	time.Sleep(0.5 * 1e9)
+	for {
+		input = <-ch
+		fmt.Printf("%s\n", input)
+	}
+}
