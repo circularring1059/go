2cc760a2f1a414dccdd8520904717d5743026c2c
原件 <ring@163.com>
2022-01-15 12:38:55 +0800

    update

--- leetcode/onetwothree/main.go
+++ leetcode/onetwothree/main.go
@@ -1,29 +1,18 @@
 package main
 
-import "fmt"
-
-// func s(dig int) {
-// 	// digArry = append(digArry, dig%10)
-// 	// if dig/10 != 0 {
-// 	// 	s(dig / 10)
-// 	// }
-// }
+import (
+	"fmt"
+	"strconv"
+)
 
 func oneTwo(dig int) []int {
-	// fmt.Println(dig % 10)
-	// if dig/10 != 0 {
-	// 	oneTwo(dig / 10)
-	// }
 	digArry := make([]int, 0)
 	s := func(a int) (c, d int) {
-		// a := dig / 10
-		// b := dig % 10
 		return a / 10, a % 10
 
 	}
 	e, b := dig, 0
 	for {
-
 		e, b = s(e)
 		digArry = append(digArry, b)
 		if e == 0 {
@@ -31,9 +20,9 @@ func oneTwo(dig int) []int {
 		}
 	}
 	return digArry
-
 }
 func main() {
 	fmt.Println("one two three")
-	fmt.Print(oneTwo(123))
+	fmt.Println(oneTwo(123))
+	fmt.Println([]byte(strconv.Itoa(123)))
 }
