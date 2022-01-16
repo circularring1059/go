33566640e318a3271707983b28f5f5094e412dd9
原件 <ring@163.com>
2022-01-16 11:11:00 +0800

    update

--- leetcode/link/main.go
+++ leetcode/link/main.go
@@ -17,22 +17,82 @@ func newLink() *link {
 	return &link{nil}
 }
 
-// func (node *node) add(data interface{}) {
-// 	new_node := node{12, nil}
-// 	node.node = &new_node
+func (link *link) is_empty() bool {
+	if link.node != nil {
+		return false
+	}
+	return true
+}
 
-// }
+func (link *link) addNode(data interface{}) {
+	newNode := &node{data, nil}
+	if !link.is_empty() {
+		cur := link.node
+		newNode.node = cur
+		link.node = newNode
+	}
+	link.node = newNode
+}
 
-// func (node *node) is_empty() bool {
-// 	if node.node == nil {
-// 		return true
+func (link *link) append(data interface{}) {
+	newNode := &node{data, nil}
+	if !link.is_empty() {
+		cur := link.node
+		for cur.node != nil {
+			cur = cur.node
+		}
+		cur.node = newNode
+	} else {
+		link.node = newNode
+	}
+}
+
+func (link *link) length() int {
+	if link.is_empty() {
+		return 0
+	}
+	count := 1
+	cur := link.node
+	for cur.node != nil {
+		cur = cur.node
+		count++
+	}
+	return count
+
+}
+
+// func (link *link) insert(index int, data interface{}) {
+// 	newNode := &node{data, nil}
+// 	if !link.is_empty() {
+// 		cur := link.node
+// 		for cur.node != nil {
+// 			cur = cur.node
+// 		}
+// 		cur.node = newNode
+// 	} else {
+// 		link.node = newNode
 // 	}
-// 	return false
 // }
+
+func (link *link) travel() {
+	cur := link.node
+	if cur != nil {
+		for cur.node != nil {
+			fmt.Println(cur.data)
+			cur = cur.node
+		}
+		fmt.Println(cur.data)
+	}
+}
 
 func main() {
 
 	link := newLink()
-	fmt.Println(link)
-
+	link.addNode("ring")
+	link.append("file")
+	link.append("dir")
+	link.addNode("yuan")
+	link.addNode("yuan")
+	link.travel()
+	fmt.Println(link.length())
 }
