47fee3e411e1598fcdc05b519b031ba0615d73db
yuanjiancheng <yuan.jiancheng@ricequant.com>
2022-01-18 10:17:29 +0800

    update

--- base/packageOfBufio/main.go
+++ base/packageOfBufio/main.go
@@ -43,3 +43,39 @@ func main() {
 	fmt.Println(string(text))
 
 }
+
+func fileWrte() {
+	file, err := os.OpenFile("./write.txt", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+	if err != nil {
+		fmt.Println("open file failed, err:", err)
+		return
+	}
+	defer file.Close()
+	str := "要写入的的text"
+	file.Write([]byte(str))     //写入字节切片数据
+	file.WriteString("content") //直接写入字符串数据
+}
+
+func bufioWrite() {
+	file, err := os.OpenFile("./bufio.txt", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+	if err != nil {
+		fmt.Println("open file failed, err:", err)
+		return
+	}
+	defer file.Close()
+	writer := bufio.NewWriter(file)
+	for i := 0; i < 10; i++ {
+		writer.WriteString("a line\n") //将数据先写入缓存
+	}
+	writer.Flush() //将缓存中的内容写入文件
+
+}
+
+func ioutilWrite() {
+	str := "content write on disk"
+	err := ioutil.WriteFile("./iotuilWrte.txt", []byte(str), 0666)
+	if err != nil {
+		fmt.Println("write file failed, err:", err)
+		return
+	}
+}
