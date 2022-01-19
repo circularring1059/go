df64f81a4b7cb20ea92c7eaa088176d5dd26b938
yuanjiancheng <yuan.jiancheng@ricequant.com>
2022-01-19 12:08:26 +0800

    update

--- base/template/day1/main.go
+++ base/template/day1/main.go
@@ -6,10 +6,17 @@ import (
 	"text/template"
 )
 
+type person struct {
+	Name string
+	Age  int
+}
+
 func main() {
+	ring := person{"ring", 18}
+
 	name := "ring"
 	//模板
-	temp := "my name is {{.}}"
+	temp := "my name is {{.}}\n"
 	tmpl, err := template.New("test").Parse(temp)
 	if err != nil {
 		fmt.Println("template failed")
@@ -18,6 +25,14 @@ func main() {
 	err = tmpl.Execute(os.Stdout, name)
 	if err != nil {
 		fmt.Println("template execute  failed")
+		return
+	}
+	temp1 := "my name is {{.Name}},  age is {{.Age}}"
+	tmpl1, err := template.New("test").Parse(temp1)
+	err = tmpl1.Execute(os.Stdout, ring)
+	if err != nil {
+		fmt.Println("template execute failed")
+		fmt.Println(err)
 		return
 	}
 
