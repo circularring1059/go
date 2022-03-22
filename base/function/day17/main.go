a32d8b050ce5f207da426ca596b89f9e7718be4b
yuanjiancheng <yuan.jiancheng@ricequant.com>
2022-03-22 10:40:25 +0800

    update

--- base/function/day17/main.go
+++ base/function/day17/main.go
@@ -6,10 +6,20 @@ func add(x, y int) int {
 	return x + y
 }
 
+//函数作为参数
 func ops(x, y int, ops func(int, int) int) {
 	fmt.Println(ops(x, y))
 }
 
+//函数作为返回值
+func ops1(x, y int) func(int, int) int {
+	fmt.Println(x, y)
+	fmt.Println("return function")
+	return add
+}
+
 func main() {
 	ops(2, 8, add)
+	funcIns := ops1(1, 9)
+	fmt.Println(funcIns(1, 3))
 }
