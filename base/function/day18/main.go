295b539829a267ac5e2472f4ff97d709d997720b
yuanjiancheng <yuan.jiancheng@ricequant.com>
2022-03-22 11:49:18 +0800

    update

--- base/function/day18/main.go
+++ base/function/day18/main.go
@@ -21,6 +21,19 @@ func adder1(x int) func(x int) int {
 	}
 }
 
+func calc(base int) (func(int) int, func(int) int) {
+	add := func(i int) int {
+		base += i
+		return base
+	}
+
+	sub := func(i int) int {
+		base -= i
+		return base
+	}
+	return add, sub
+}
+
 func main() {
 	adderIns := adder()
 	fmt.Println(adderIns(9)) //9
@@ -33,4 +46,10 @@ func main() {
 	adder2Ins := adder1(9)
 	fmt.Println(adder2Ins(9)) //18
 	fmt.Println(adder2Ins(9)) //27
+
+	f1, f2 := calc(10)
+	fmt.Println(f1(1), f2(2)) //11 9
+	fmt.Println(f1(3), f2(4)) //12 8
+	fmt.Println(f1(5), f2(6)) //13 7
+
 }
