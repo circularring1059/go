526542bf70cb54663265f3e2dbe5bdb12315f0c2
yuanjiancheng <yuan.jiancheng@ricequant.com>
2022-03-22 10:52:57 +0800

    update

--- base/function/day17/main.go
+++ base/function/day17/main.go
@@ -22,4 +22,15 @@ func main() {
 	ops(2, 8, add)
 	funcIns := ops1(1, 9)
 	fmt.Println(funcIns(1, 3))
+
+	//匿名函数
+	sub := func(x, y int) int {
+		return x - y
+	}
+	fmt.Println(sub(7, 2))
+
+	func(x, y int) {
+		fmt.Println(x * y)
+	}(2, 8)
+
 }
