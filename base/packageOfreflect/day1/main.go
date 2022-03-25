02418e9e2e579daf550ce547136150753ac045c1
yuanjiancheng <yuan.jiancheng@ricequant.com>
2022-03-25 16:46:45 +0800

    update

--- base/packageOfreflect/day1/main.go
+++ base/packageOfreflect/day1/main.go
@@ -5,6 +5,18 @@ import (
 	"reflect"
 )
 
+type person struct {
+	age  int
+	name string
+}
+
+func createPerson(age int, name string) *person {
+	return &person{
+		age,
+		name,
+	}
+}
+
 func main() {
 	fmt.Println("package of reflect")
 
@@ -24,4 +36,15 @@ func main() {
 	fmt.Println(reflect.TypeOf(f)) // float64
 	fmt.Println(reflect.TypeOf(g)) // float64
 
+	//Go语言的反射中像数组、切片、Map、指针等类型的变量，它们的.Name()都是返回空。
+	alice := createPerson(13, "alice")
+	typeOfAlice := reflect.TypeOf(alice)
+	fmt.Println(reflect.TypeOf(alice))
+	fmt.Printf("Name:%v, Kind:%v\n", typeOfAlice.Name(), typeOfAlice.Kind()) //Name:, Kind:ptr
+
+	var bobo interface{} = person{18, "bobo"}
+	typeOfBobo := reflect.TypeOf(bobo)
+	fmt.Println(reflect.TypeOf(bobo))                                      //main.person
+	fmt.Println(typeOfBobo.Name(), typeOfBobo.Kind())                      //person struct
+	fmt.Printf("Name:%v, Kind:%v\n", typeOfBobo.Name(), typeOfBobo.Kind()) //Name:person, Kind:struct
 }
