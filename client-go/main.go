bd51f28fd5e86aa73393e729ebbf87dbbeb70ac0
yuanjiancheng <yuan.jiancheng@ricequant.com>
2022-03-29 09:08:29 +0800

    update

--- client-go/main.go
+++ client-go/main.go
@@ -5,9 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"path/filepath"
-	"time"
 
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -43,30 +41,26 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	for {
-		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 
-		// Examples for error handling:
-		// - Use helper functions like e.g. errors.IsNotFound()
-		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		namespace := "default"
-		pod := "example-xxxxx"
-		_, err = clientset.CoreV1().Pods(namespace).Get(context.TODO(), pod, metav1.GetOptions{})
-		if errors.IsNotFound(err) {
-			fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
-		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-			fmt.Printf("Error getting pod %s in namespace %s: %v\n",
-				pod, namespace, statusError.ErrStatus.Message)
-		} else if err != nil {
-			panic(err.Error())
-		} else {
-			fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
-		}
-
-		time.Sleep(10 * time.Second)
+	//获取default namespace 下的所有pod
+	pods, err := clientset.CoreV1().Pods("default").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic("list pod faile")
 	}
+
+	//fmt.Println(len(pods.Items))
+
+	for _, pod := range pods.Items {
+		fmt.Println(pod.Name)
+	}
+
+	//获取单个pod
+	pod, err := clientset.CoreV1().Pods("default").Get(context.TODO(), "etcd-56f5bf55b6-vqvjc", metav1.GetOptions{})
+
+	if err != nil {
+		fmt.Println("not such pod")
+	} else {
+		fmt.Println("pod:", pod.Labels)
+	}
+
 }
