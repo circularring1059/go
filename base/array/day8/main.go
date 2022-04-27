f0695d207bcc89f3899d0bd56f9e6d0170bebebe
元件 <56504340+circularring1059@users.noreply.github.com>
2022-04-27 14:57:26 +0800

    update

--- base/array/day8/main.go
+++ base/array/day8/main.go
@@ -6,10 +6,20 @@ func main() {
 	sl1 := []int{1, 2, 3}
 	changeSlice(sl1)
 	fmt.Println(sl1)
+
+	sl2 := []int{1, 2, 3}
+	changeSlice1(&sl2)
+	fmt.Println("sl2 is:", sl2)
+
 }
 
-func changeSlice(s []int) {  //slice  引用传递
+func changeSlice(s []int) { //slice  引用传递
 	// s = append(s, 0)
 	s[0] = 4
 	fmt.Println(s)
+}
+
+func changeSlice1(s *[]int) { //使用append 时需要传指针才能正确修改
+	*s = append(*s, 2)
+	fmt.Println(*s)
 }
