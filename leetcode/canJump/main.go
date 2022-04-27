59f03a4ed9d8ebb41aec56f7956137c5ecce172b
元件 <56504340+circularring1059@users.noreply.github.com>
2022-04-27 11:28:52 +0800

    update

--- leetcode/canJump/main.go
+++ leetcode/canJump/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func canJump(num []int) bool {
+	if len(num) <= 1 {
+		return true
+	}
 	max := num[0]
 	if num[0] <= 0 {
 		return false
@@ -26,8 +31,40 @@ func canJump(num []int) bool {
 }
 
 func main() {
-	num := []int{2, 0, 0, 0, 7}
+	num := []int{2, 0, 2, 0, 7}
 	num1 := []int{}
 	fmt.Println(canJump(num))
 	fmt.Println(canJump(num1))
+	fmt.Println(Solution(num1))
+	fmt.Println(Solution(num))
+}
+
+func Solution(s []int) bool {
+	if len(s) <= 1 {
+		return false
+	}
+
+	if s[0] == 0 {
+		return true
+	}
+	max_jump := s[0]
+
+	for index, val := range s {
+		if max_jump < index {
+			return false
+		}
+		max_jump = max(max_jump, index+val)
+		if max_jump >= len(s)-1 {
+			return true
+		}
+	}
+	return false
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	} else {
+		return y
+	}
 }
