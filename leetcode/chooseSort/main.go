71d8922ba9cd94469fdb37b3e742310f67610e21
元件 <56504340+circularring1059@users.noreply.github.com>
2022-04-27 14:00:25 +0800

    update

---
+++ leetcode/chooseSort/main.go
@@ -0,0 +1,30 @@
+package main 
+
+import "fmt"
+
+func main () {
+	fmt.Println("start job")
+	sl := []int{2,7,9}
+	fmt.Println(Solution(sl))  //[9 7 2]
+	fmt.Println(sl) //[9 7 2]
+
+}
+
+
+func Solution(s []int) []int{
+	if len(s) <= 1 {
+		return s
+	}
+
+	for i := 0; i < len(s); i++ {
+		middle_index := i
+		for j := i;  j < len(s); j ++{
+			//找到最大的那个数的索引
+			if s[j] > s[middle_index]{
+				middle_index = j
+			}
+		}
+		s[i], s[middle_index] = s[middle_index], s[i]
+	}
+	return s
+}
