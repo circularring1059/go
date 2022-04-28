45cee3887159e2c0e4c529de359b0ec1862fea63
元件 <56504340+circularring1059@users.noreply.github.com>
2022-04-28 09:55:54 +0800

    update

--- leetcode/reverseLink/main.go
+++ leetcode/reverseLink/main.go
@@ -4,8 +4,14 @@ import "fmt"
 
 func main () {
 	fmt.Println("start job")
-	node1 := newNode(8)
-	fmt.Println(node1)
+	node1 := newNode(1)
+	node2 := newNode(2)
+	node3 := newNode(3)
+	node1.node = node2
+	node2.node = node3
+	node1.travel()
+	reverLink(node1)
+	node3.travel()
 }
 
 type Node struct {
@@ -16,7 +22,34 @@ type Node struct {
 
 func newNode(i interface{}) *Node{
 	return &Node{
-		val i,
-		node nil,
+		val: i,
+		node: nil,
+	}
+}
+
+func (n *Node) travel(){
+	node := n
+	for node != nil{
+		fmt.Println(node.val)
+		node = node.node
+	}
+}
+
+func reverLink(n *Node) Node{
+	if n.node == nil{
+		return *n
+	}else{
+		//进行reverse 后进行遍历会多出这个最后面node的 0 值
+		pre := &Node{
+			val: 0,
+		    node: nil,
+		}
+		for n != nil{
+			next_node := n.node
+			n.node = pre
+			pre = n
+			n = next_node
+		}
+		return *pre
 	}
 }
