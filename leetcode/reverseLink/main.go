8d90085fa77906d5b0e7b73ea8844c981d88fc10
元件 <56504340+circularring1059@users.noreply.github.com>
2022-04-28 10:54:02 +0800

    update

--- leetcode/reverseLink/main.go
+++ leetcode/reverseLink/main.go
@@ -6,19 +6,16 @@ func main () {
 	fmt.Println("start job")
 	node1 := newNode(1)
 	node2 := newNode(2)
-	node3 := newNode(3)
 	node1.node = node2
-	node2.node = node3
 	node1.travel()
-	reverLink(node1)
-	node3.travel()
+	// reverseLink1(node1)
+	// node2.travel()
 }
 
 type Node struct {
 	val interface{}
 	node *Node
 }
-
 
 func newNode(i interface{}) *Node{
 	return &Node{
@@ -30,7 +27,12 @@ func newNode(i interface{}) *Node{
 func (n *Node) travel(){
 	node := n
 	for node != nil{
-		fmt.Println(node.val)
+		
+		if node.node == nil {
+			fmt.Println(node.val, "-> nil")
+			return 
+		}
+		fmt.Print(node.val, "-> ")
 		node = node.node
 	}
 }
@@ -40,10 +42,11 @@ func reverLink(n *Node) Node{
 		return *n
 	}else{
 		//进行reverse 后进行遍历会多出这个最后面node的 0 值
-		pre := &Node{
-			val: 0,
-		    node: nil,
-		}
+		// pre := &Node{
+		// 	val: 0,
+		//     node: nil,
+		// }
+		var pre *Node = nil
 		for n != nil{
 			next_node := n.node
 			n.node = pre
@@ -51,5 +54,27 @@ func reverLink(n *Node) Node{
 			n = next_node
 		}
 		return *pre
+	}
+}
+
+func reverseLink1(n *Node) Node{
+	node := n
+	if node.node == nil {
+		return *node
+	}
+	*node = backtack(node)
+	return *node
+}
+
+func backtack(n *Node) Node {
+	//到最后一个node
+	node := n
+	if node.node == nil {
+		return *node
+	}else{
+		newNode := backtack(node.node)
+		node.node.node = node 
+		node.node = nil
+		return newNode
 	}
 }
