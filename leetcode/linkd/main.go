f8de2c463e13f9656d399aa9aa2826da23d5ee24
元件 <56504340+circularring1059@users.noreply.github.com>
2022-04-29 12:08:44 +0800

    update

--- leetcode/linkd/main.go
+++ leetcode/linkd/main.go
@@ -20,8 +20,11 @@ func main() {
 	link.append(2)
 	link.append(3)
 	link.append(4)
+	link.insert(6, 9)
 	link.travel()
-	fmt.Println("link lenght :", link.length())
+	link.reverse1()
+	link.travel()
+	// fmt.Println("link lenght :", link.length())
 }
 
 func newNode(val interface{}) *Node {
@@ -74,3 +77,61 @@ func (L *Link) travel() {
 	}
 }
 
+func (L *Link) insert(val interface{}, index int) {
+	if index < 0 {
+		fmt.Println("worring: index is lt 0 setdefault 0")
+		index = 0
+	}
+	if index > L.length()-1 {
+		fmt.Println("worring:", val, "out of index setdefautl max_index:", L.length())
+		index = L.length() - 1
+	}
+	new_node := newNode(val)
+	if L.head == nil {
+		L.head = new_node
+	} else if index == L.length()-1 {
+		L.append(val)
+	} else if index == 0 {
+		new_node.node = L.head
+		L.head = new_node
+	} else {
+		cur := L.head
+		for count := 1; count < index; count++ {
+			cur = cur.node
+		}
+		new_node.node = cur.node
+		cur.node = new_node
+	}
+}
+
+func (L *Link) reverse() {
+	var pre *Node = nil
+	cur := L.head
+	for cur != nil {
+		next_node := cur.node
+		cur.node = pre
+		pre = cur
+		cur = next_node
+	}
+	L.head = pre
+}
+
+func (L *Link) reverse1() {
+	if L.head == nil || L.head.node == nil {
+		return
+	}
+	cur := L.head
+	cur = backtack(cur)
+	L.head = cur
+}
+
+func backtack(N *Node) *Node {
+	node := N
+	if node.node == nil {
+		return node
+	}
+	newNode := backtack(node.node)
+	node.node.node = node
+	node.node = nil
+	return newNode
+}
