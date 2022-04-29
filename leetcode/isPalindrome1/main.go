235fdf2470efce7d86081b950ee787132e550e8b
元件 <56504340+circularring1059@users.noreply.github.com>
2022-04-29 18:13:04 +0800

    update

--- leetcode/isPalindrome1/main.go
+++ leetcode/isPalindrome1/main.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("start job")
 	fmt.Println(isPalindrome("ringgnir"))
+	fmt.Println(isPalindrome1("ring"))
 }
 
 func isPalindrome(str string) bool {
@@ -12,8 +13,24 @@ func isPalindrome(str string) bool {
 	if len(str_arry) <= 1 {
 		return true
 	}
-    if str_arry[0] != str_arry[len(str_arry)-1] {
+	if str_arry[0] != str_arry[len(str_arry)-1] {
 		return false
 	}
-	return isPalindrome(string(str_arry)[1: len(str_arry)-1])
-}	
+	return isPalindrome(string(str_arry)[1 : len(str_arry)-1])
+}
+
+func isPalindrome1(str string) bool {
+	if len(str) <= 1 {
+		return true
+	}
+
+	left, right := 0, len(str)-1
+	for left < right {
+		if []byte(str)[left] != []byte(str)[right] {
+			return false
+		}
+		left++
+		right--
+	}
+	return true
+}
