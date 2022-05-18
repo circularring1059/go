fea4b76032a6eb8cd3d20a1ff1701b852052e7e9
元件 <56504340+circularring1059@users.noreply.github.com>
2022-05-18 17:33:33 +0800

    update

--- base/array/day7/main.go
+++ base/array/day7/main.go
@@ -7,7 +7,12 @@ func main() {
 	slice2 := []int{}
 	slice3 := []int{}
 	fmt.Println(slice1, slice2)                        //[] []
-	fmt.Printf("%p, %p, %p\n", slice1, slice2, slice3) //0x0, 0xcf6d60, 0xcf6d60
+	fmt.Printf("%p, %p, %p\n", slice1, slice2, slice3) //0x0, 0xcf6d60, 0xcf6d60  一样
 	slice1 = append(slice1, []int{4, 5}...)            //append can not init
 	fmt.Println(slice1)
+
+	map1 := make(map[int]int, 0)
+	map2 := make(map[int]int, 0)
+	fmt.Printf("%p, %p", map1, map2) //0xc000076480, 0xc0000764b0 不一样
+
 }
