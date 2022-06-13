04be9d2c10b0cec86ae3d963f7b0bd65701520a2
元件 <56504340+circularring1059@users.noreply.github.com>
2022-06-13 10:17:40 +0800

    update

--- leetcode/twoSum/main.go
+++ leetcode/twoSum/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("study")
 	var sl = []int{1, 2, 4}
-	fmt.Println(twoSun(sl, 6))
+	fmt.Println(twoSun1(sl, 3))
 }
 
 func twoSun(nums []int, target int) [][]int {
@@ -13,12 +13,25 @@ func twoSun(nums []int, target int) [][]int {
 	mp := make(map[int]int)
 
 	for i := 0; i < len(nums); i++ {
-		_, ok := mp[target-nums[i]]
-		if ok {
+		//_, ok := mp[target-nums[i]]
+		if _, ok := mp[target-nums[i]]; ok {
 			sl = append(sl, []int{mp[target-nums[i]], i})
 		}
 		mp[nums[i]] = i
 	}
 	return sl
 
+}
+
+func twoSun1(nums []int, target int) [][]int {
+	sl := make([][]int, 0)
+	//mp := make(map[int]int, 0)
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if target == nums[j]+nums[i] {
+				sl = append(sl, []int{i, j})
+			}
+		}
+	}
+	return sl
 }
